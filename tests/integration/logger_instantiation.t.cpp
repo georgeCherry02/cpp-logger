d51a9a537d4072bbee724f6ae9b131549b13ca1a
GeorgeCherry02 <georgeb.cherry@gmail.com>
2023-03-15 20:52:25 +0000

    Add date-time regex to tests

--- tests/integration/logger_instantiation.t.cpp
+++ tests/integration/logger_instantiation.t.cpp
@@ -1,9 +1,11 @@
 #include <logger.h>
+#include <logging_level.h>
 
 #include <catch2/catch_test_macros.hpp>
 #include <fstream>
 #include <iostream>
 #include <optional>
+#include <regex>  // If you solve a problem with regex you now have two problems...
 #include <sstream>
 #include <string>
 
@@ -11,20 +13,61 @@ namespace logger {
 namespace test {
 
 namespace utils {
-bool validate_message_written(const std::string& file_path,
-                              const std::string& expected_message) {
+
+bool validate_log_file(const std::string& file_path,
+                       std::vector<std::regex>&& expected_logs) {
     std::ifstream fh{file_path};
     if (!fh.is_open()) {
+        std::cout << "Failed to read in file when testing, path provided="
+                  << file_path << std::endl;
         return false;
     }
+
     std::string output;
-    std::cout << "Reading in file:" << std::endl;
+    bool still_matches{true};
+
+    std::cout << "Reading in a file:" << std::endl;
+    int target_line = 0;
     while (std::getline(fh, output)) {
         std::cout << output << std::endl;
-        return expected_message == output;
+
+        if (target_line > expected_logs.size()) {
+            std::cout << "Expected more lines than were present in the file"
+                      << std::endl;
+            return false;
+        }
+
+        bool matches{std::regex_match(output, expected_logs.at(target_line))};
+        if (!matches) {
+            std::cout << "Line mismatch! In file: " << output << std::endl;
+        }
+
+        if (still_matches && !matches) {
+            still_matches = false;
+        }
+
+        ++target_line;
     }
-    return false;
+    return still_matches;
 };
+
+bool validate_single_message_written(const std::string& file_path,
+                                     LoggingLevel&& level,
+                                     const std::string& expected_message) {
+    std::stringstream ss;
+    ss << "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}.\\d{3} "
+       << expected_message << "$";
+    std::regex r{ss.str()};
+    return validate_log_file(file_path, {r});
+};
+
+std::regex construct_expected_log_line(const LoggingLevel& level,
+                                       const std::string& message) {
+    std::stringstream ss;
+    ss << "*" << message;
+    return std::regex{ss.str()};
+};
+
 }  // namespace utils
 
 class FileHandlingFixture {
@@ -63,7 +106,8 @@ SCENARIO("Outputting Data") {
         WHEN("`output` is called on said `Logger`") {
             logger->output(message);
             THEN("The file being written to contains the message") {
-                CHECK(utils::validate_message_written(file_path, message));
+                CHECK(utils::validate_single_message_written(
+                    file_path, LoggingLevel::INFO, message));
             }
         }
     }
